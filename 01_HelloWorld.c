da21fcf985d41b1cf12010025fe1c83154228b5a
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-01-16 02:55:52 +0000

    Commit

--- 01_HelloWorld.c
+++ 01_HelloWorld.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void) {
+int hello_world(void) {
   printf("Hello World\n");
   return 0;
 }
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+   int healt = 100;
+   printf(healt);
+
+    return 0;
+}
