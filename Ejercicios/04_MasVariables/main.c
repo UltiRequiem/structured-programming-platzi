4063e8b7912582c2afacf66fd48a576e844e4dac
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-01-19 20:31:04 -0500

    Inputs

--- Ejercicios/04_MasVariables/main.c
+++ Ejercicios/04_MasVariables/main.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
 
 int main()
 {
---
+++ Ejercicios/05_Inputs/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Entrada de datos Parte 2\n\n");
+
+    int enteroA;
+    float flotanteA;
+    char caracterA;
+
+
+    printf("Ingresa el valor de enteroA es: ");
+    scanf("%i", &enteroA);
+
+    printf("Ingresa el valor de flotanteA es: ");
+    scanf("%f", &flotanteA);
+
+    printf("Ingresa el valor de caracterA es: ");
+    scanf(" %c", &caracterA);
+
+    printf("\n\n");
+    printf("enteroA: %d\n", enteroA);
+    printf("flotanteA: %f\n", flotanteA);
+    printf("caracterA: %c\n", caracterA);
+
+    return 0;
+}
