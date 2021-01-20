726d8325fde06d0580d454bf3c1f65850fe07168
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-01-20 11:27:51 -0500

    Operadores Aritmeticos

---
+++ Ejercicios/06_OperadoresAritmeticos/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int rSum;
+    float rRes;
+    float rDiv;
+    float rMult;
+    int rMod;
+
+    rSum = 789 + 987;
+    rRes = 7877.5878996 - 799.5578;
+    rDiv = 7.5844336 / 799557788;
+    rMult = 7.584446 * 7944478;
+    rMod = 7%3;
+
+    printf("Resultado de la suma: %i \n" ,rSum);
+    printf("Resultado de la resta: %f \n" ,rRes);
+    printf("Resultado de la division: %f \n" ,rDiv);
+    printf("Resultado de la multiplicacion: %f \n" ,rMult);
+    printf("Resultado del modulo: %i \n" ,rMod);
+
+
+    return 0;
+}
