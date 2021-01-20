3978058e2e86261c48a67f1f27dec5e735b0e368
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-01-19 20:12:06 -0500

    Mas variables

---
+++ Ejercicios/03_Variables_y_tipos_de_Datos/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int healthpoints = 100;
+    float damage = 13.05;
+    char favoriteLetter = 'D'
+    bool hasFun = false;
+
+    return 0;
+}
