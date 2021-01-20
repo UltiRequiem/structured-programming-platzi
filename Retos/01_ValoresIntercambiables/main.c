7e537fbdfb57c6801ff0f4db232f246f6d8b6175
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-01-19 20:44:17 -0500

    Primer Reto Resuelto :D

---
+++ Retos/01_ValoresIntercambiables/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int X;
+    int Y;
+    int intercambiador;
+
+    printf("(Solo Ingresar numeros Porfavor) \n");
+    printf("Da el valor de x: ");
+    scanf("%i: ", &X);
+
+    printf("Da el valor de y: ");
+    scanf("%i: ", &Y);
+
+    intercambiador = X;
+    X = Y;
+    Y = intercambiador;
+
+    printf("\nAhora X vale: %i \n", X);
+    printf("Ahora Y vale: %i \n\n", Y);
+    // XD
+    printf("Salu2 \n");
+
+    return 0;
+}
