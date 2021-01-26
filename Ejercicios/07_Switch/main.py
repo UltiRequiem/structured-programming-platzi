6718e609b886abdfc91fb930c38ab0920dabdcfa
Zer <eliaz.bobadilladev@gmail.com>
2021-01-26 14:22:18 -0500

    Avanze

---
+++ Ejercicios/07_Switch/main.py
@@ -0,0 +1,17 @@
+def main():
+    print('07_Switch')
+
+    x = int(input("Ingresa un numero: "))
+
+    if x == 1:
+        print("\nElegiste el numero uno.\n\n")
+    elif x == 2:
+        print("\nElegiste el numero dos.\n\n")
+    elif x == 3:
+        print("\nElegiste el numero tres.\n\n")
+    else:
+        print('\nElegiste un numero raro.\n')
+
+
+if __name__ == '__main__':
+    main()
