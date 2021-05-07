74ac0d932545ed370c854d49c937a042e4617678
Eliaz Bobadilla <71897736+UltiRequiem@users.noreply.github.com>
2021-05-07 10:00:09 -0500

    Update main.py

--- Retos/01_ValoresIntercambiables/main.py
+++ Retos/01_ValoresIntercambiables/main.py
@@ -2,13 +2,10 @@ def main():
   X = input('Da el valor a X: ')
   Y = input('Da el valor a Y: ')
 
-  intercambiador = X
-  X = Y
-  Y = intercambiador
+  Y, X = X, Y
     
   print("\nAhora X vale: "+ X)
   print("Ahora Y vale: "+ Y)
-  print("Salu2") #XD
 
 if __name__ == "__main__":
     main()
